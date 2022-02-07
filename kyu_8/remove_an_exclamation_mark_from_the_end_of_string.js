269503eeb03e70356dc1120278615d00b92412e7
pu4ick <98850988+pu4ick@users.noreply.github.com>
2022-02-07 13:04:35 +0100

    Remove an exclamation mark from the end of string (other answers)

--- kyu_8/remove_an_exclamation_mark_from_the_end_of_string.js
+++ kyu_8/remove_an_exclamation_mark_from_the_end_of_string.js
@@ -1,3 +1,9 @@
 function remove (string) {
   return string[string.length - 1] === "!" ? string.slice(0,string.length - 1) : string;
 } 
+
+// const remove = s => s.replace(/!$/, '');
+
+// function remove(s) {
+//   return s.endsWith('!') ? s.slice(0, -1) : s;
+// }
